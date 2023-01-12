14acfc4eb882f214c56c63e3bc91d0fdf681c7c5
Zeddy D <30397166+zdafla@users.noreply.github.com>
2023-01-12 14:14:52 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.blog.ami
+  value = aws_instance.web.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.blog.arn
+  value = aws_instance.web.arn
 }
