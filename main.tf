de776072eca9ba8b66c9572e06033eb390e6046e
Zeddy D <30397166+zdafla@users.noreply.github.com>
2023-01-15 00:17:22 -0500

    dev correction

--- main.tf
+++ main.tf
@@ -45,8 +45,8 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
-  name    = "blog"
 
+  name    = "blog"
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules        = ["http-80-tcp","https-443-tcp"]
